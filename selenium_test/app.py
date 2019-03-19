141995f0d8e85c5ba6b9b5e923f011e7709ef23a
RoraxHan(kyl) <hkg328@outlook.com>
2019-03-19 08:10:11 -0400

    scrap spa with selenium

--- selenium_test/app.py
+++ selenium_test/app.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import os
+import codecs
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
@@ -7,10 +11,38 @@ from selenium.common.exceptions import TimeoutException
 
 
 
-chrome_driver_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'chromedriver'))
-print(chrome_driver_path)
-browser = webdriver.Chrome(chrome_driver_path) #replace with .Firefox(), or with the browser of your choice
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+chrome_driver_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'chromedrivers/mac/chromedriver'))
+
+
+browser = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=option) #replace with .Firefox(), or with the browser of your choice
 # browser = webdriver.Firefox()
-url = "http://example.com/login.php"
+url = "https://www.onlinedoctor.ch/de/dermatologie-zurich/"
 browser.get(url) #navigate to the page
-browser.close()
+
+try:
+
+  # Wait 20 seconds for page to load
+  # timeout = 20
+  # WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full rounded-2']")))
+  # # find_elements_by_xpath returns an array of selenium objects.
+  # titles_element = browser.find_elements_by_xpath("//a[@class='text-bold']")
+
+  # # use list comprehension to get the actual repo titles and not the selenium objects.
+  # titles = [x.text for x in titles_element]
+  # # print out all the titles.
+  # print('titles:')
+  # print(titles, '\n')
+
+
+  source_code = browser.execute_script("return document.body.parentElement.outerHTML") #returns the inner HTML as a string
+  file = codecs.open("scraped.html", "w", "utf-8")
+  file.write(source_code)
+  file.close()
+  
+
+  browser.close()
+except TimeoutException:
+  print("Timed out waiting for page to load")
+  browser.quit()
