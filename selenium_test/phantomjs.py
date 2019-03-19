9b3da14db8a7aeb8281b27f5fae3249fbb4555c4
RoraxHan(kyl) <hkg328@outlook.com>
2019-03-19 13:08:15 -0400

    selenium phantomjs deprecated

---
+++ selenium_test/phantomjs.py
@@ -0,0 +1,3 @@
+
+# UserWarning: Selenium support for PhantomJS has been deprecated, please use headless versions of Chrome or Firefox instead
+# warnings.warn('Selenium support for PhantomJS has been deprecated, please use headless '
