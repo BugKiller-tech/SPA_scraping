82f6ae27614f0a60cb81f84cecaa5ff3c1d0c6e1
RoraxHan(kyl) <hkg328@outlook.com>
2019-03-20 05:19:46 -0400

    set some setting for selenium headless

--- selenium_test/headless.py
+++ selenium_test/headless.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import datetime
 import codecs
 
 from selenium import webdriver
@@ -9,27 +10,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
 
-def write_to_file(text):
+def write_to_file(source_code):
   file = codecs.open("scraped.html", "w", "utf-8")
   file.write(source_code)
   file.close()
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
+def selenium_options():
+  options = webdriver.ChromeOptions()
+  options.add_argument('headless')
+  return options
+
 chrome_driver_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'chromedrivers/mac/chromedriver'))
-browser = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=options) #replace with .Firefox(), or with the browser of your choice
+def scrap_url(url):
+  try:
+    options = selenium_options()
+    driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=options) #replace with .Firefox(), or with the driver of your choice
+    print('implicity wait', datetime.datetime.now())
+    driver.implicitly_wait(30)
+    print('end of implicity wait', datetime.datetime.now())
+    driver.get(url) #navigate to the page
+    # source_code = driver.execute_script("return document.body.parentElement.outerHTML") #returns the inner HTML as a string
+    source_code = driver.page_source
+    driver.close()
+    write_to_file(source_code)
+    print('completed scraping with headless browser')
+  except TimeoutException:
+    print("Timed out waiting for page to load")
+  finally:
+    driver.quit()
 
-try:
-  url = "https://bvopen.abrickis.me/#/standings"
-  browser.get(url) #navigate to the page
 
-  source_code = browser.execute_script("return document.body.parentElement.outerHTML") #returns the inner HTML as a string
-  browser.close()
-  
-  write_to_file(source_code)
-  print('completed scraping with headless browser')
+url = "https://bvopen.abrickis.me/#/standings"
+scrap_url(url)
 
-except TimeoutException:
-  print("Timed out waiting for page to load")
-  browser.quit()
+
